ea3bdf2929b7fa2401672f89bd5f4371166b63be
Muhamad Syamim Irfan Bin Ahmad Shokkri <syamimirfan59@gmail.com>
2024-02-14 10:13:25 +0800

    First Commit

--- app/about/about-section.tsx
+++ app/about/about-section.tsx
@@ -112,7 +112,7 @@ const About_Section = () => {
                         Resume
                     </h1>
                     <Link
-                     href="https://drive.google.com/file/d/1tcveCOtArldqSW_qWvENzzfhjs5QT9cw/view?usp=sharing"
+                     href="https://drive.google.com/file/d/1h5mptCUWoHCRfXSRTcbjhq35yZVzqJpw/view?usp=sharing"
                      target="_blank"
                     >
                         <Image src="/assets/download-button.png" alt='download-button.png' width={300} height={300} className="mx-auto md:mx-0"/>
